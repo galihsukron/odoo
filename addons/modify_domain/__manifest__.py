48317fe8bf68f790ac5e960c7677cc406dc0c92a
galihsukron <galih.sukron@gmail.com>
2020-03-05 12:18:17 +0700

    [ADD] modify domain and context in view purchase order

---
+++ addons/modify_domain/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Modify domain",
+    "version": "1.0.1",
+    "category": "Purchases",
+    "author": "Galih Sukron Insani",
+    "website": "https://github.com/galihsukron",
+    "summary": "Modify domain and context in odoo",
+    "support": "galih.sukron@gmail.com",
+    "description": "How to modify domain and context in view odoo.",
+    "depends": ["purchase"],
+    "data": [
+        'views/purchase_order.xml',
+    ],
+    "license": "LGPL-3",
+    "installable": True,
+    "auto_install": False,
+}
